1cf716c2695b14f5f0a533a81f6854b3eefcad19
Álvaro Fariña <alvaro.aguinagalde13@fpuna.edu.py>
2026-04-23 14:48:17 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// EXPORTAMOS todas estas constantes para que otros archivos puedan usarlas.
+
+// --- CONFIGURACIÓN ---
+// IP, puerto y topico del mqtt
+export const brokerIp = "air-quality-campus-una.duckdns.org";
+export const brokerPort = 443;
+export const brokerPath = "/mqtt";
+export const topic = "estaciones/+/data";
+
+export const API_BASE_URL = "https://air-quality-campus-una.duckdns.org";
+
+// --- DEFINICIÓN DE COLORES ---
+export const COLOR_GREEN = '#28a745';
+export const COLOR_YELLOW = '#ffc107';
+export const COLOR_RED = '#dc3545';
+export const COLOR_CELESTE = '#17a2b8';
+export const COLOR_ORANGE = '#fd7e14';
